71821a5b3734ccdc1b8d422638d9af42c9408c09
khushii32 <khushisorout2005@gmail.com>
2026-02-24 14:51:35 +0530

    Add files via upload

---
+++ prime or not.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // notice the space before %c
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if(num2 != 0)
+                printf("Result: %.2lf\n", num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
